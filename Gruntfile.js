226d6526eaa6684b5a77654af81e5ad312d78848
Nadeem Shabir <nadeem.shabir@talis.com>
2013-12-25 21:46:24 +0000

    added jsbeautify targets to grunt file, reformats code so its all consistent

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
 
 module.exports = function (grunt) {
 
-  // Project configuration.
+    // Project configuration.
     grunt.initConfig({
         mochaTest: {
             test: {
@@ -24,6 +24,21 @@ module.exports = function (grunt) {
                 src: ['index.js']
             }
         },
+        jsbeautifier: {
+            modify: {
+                src: ['Gruntfile.js', 'index.js'],
+                options: {
+                    config: '.jsbeautifyrc'
+                }
+            },
+            verify: {
+                src: ['Gruntfile.js', 'index.js'],
+                options: {
+                    mode: 'VERIFY_ONLY',
+                    config: '.jsbeautifyrc'
+                }
+            }
+        },
         watch: {
             gruntfile: {
                 files: '<%= jshint.gruntfile.src %>',
@@ -40,7 +55,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
 
     // Default task.
     grunt.registerTask('default', ['jshint', 'mochaTest']);
+    grunt.registerTask('clean', ['jshint', 'jsbeautifier:modify']);
+    grunt.registerTask('verify', ['jshint', 'jsbeautifier:verify']);
 };
--- index.js
+++ index.js
@@ -26,31 +26,31 @@ var ERROR = "error";
  * @param config
  * @constructor
  */
-var PersonaClient = function(config) {
+var PersonaClient = function (config) {
     this.config = config = config || {};
 
     //TODO: find a less verbose way of doing this
 
-    if(this.config.persona_host === undefined){
+    if (this.config.persona_host === undefined) {
         throw new Error("You must specify the Persona server host");
     }
-    if(this.config.persona_port === undefined){
+    if (this.config.persona_port === undefined) {
         throw new Error("You must specify the Persona server port");
     }
-    if(this.config.persona_scheme === undefined){
+    if (this.config.persona_scheme === undefined) {
         throw new Error("You must specify the Persona server scheme (http/https)");
     }
-    if(this.config.persona_oauth_route === undefined){
+    if (this.config.persona_oauth_route === undefined) {
         throw new Error("You must specify the Persona oauth route");
     }
 
-    if(this.config.redis_host === undefined){
+    if (this.config.redis_host === undefined) {
         throw new Error("You must specify the Redis host to use as a cache");
     }
-    if(this.config.redis_port === undefined){
+    if (this.config.redis_port === undefined) {
         throw new Error("You must specify the Redis port");
     }
-    if(this.config.redis_db === undefined){
+    if (this.config.redis_db === undefined) {
         throw new Error("You must specify the Redis db");
     }
 
@@ -65,32 +65,35 @@ var PersonaClient = function(config) {
     this.debug("Persona Client Created");
 };
 
-PersonaClient.prototype.validateToken = function(req,res,next){
+PersonaClient.prototype.validateToken = function (req, res, next) {
     var token = this.getToken(req),
         _this = this;
 
     if (token == null) {
         res.status(401);
-        res.json({"error":"no_token","error_description":"No token supplied"});
-        throw "OAuth validation failed for "+token;
+        res.json({
+            "error": "no_token",
+            "error_description": "No token supplied"
+        });
+        throw "OAuth validation failed for " + token;
     }
 
     // if we were given a scope then append the scope to the token to create a cachekey
     var cacheKey = token;
-    if(req.param("scope")) {
+    if (req.param("scope")) {
         cacheKey += "@" + req.param("scope");
     }
 
-    this.debug("Validating token: "+cacheKey);
+    this.debug("Validating token: " + cacheKey);
 
-    this.redisClient.get("access_token:"+cacheKey,function(err,reply) {
-        if (reply==="OK") {
-            _this.debug("Token "+cacheKey+" verified by cache");
+    this.redisClient.get("access_token:" + cacheKey, function (err, reply) {
+        if (reply === "OK") {
+            _this.debug("Token " + cacheKey + " verified by cache");
             next(null, "verified_by_cache");
         } else {
 
-            var requestPath = _this.config.persona_oauth_route+token;
-            if(req.param("scope")){
+            var requestPath = _this.config.persona_oauth_route + token;
+            if (req.param("scope")) {
                 requestPath += "?scope=" + req.param("scope");
             }
 
@@ -102,40 +105,45 @@ PersonaClient.prototype.validateToken = function(req,res,next){
             };
 
             _this.debug(JSON.stringify(options));
-            _this.http.request(options,function(oauthResp) {
-                if (oauthResp.statusCode===204)
-                {
+            _this.http.request(options, function (oauthResp) {
+                if (oauthResp.statusCode === 204) {
                     // put this key in redis with an expire
-                    _this.redisClient.multi().set("access_token:"+cacheKey,'OK').expire("access_token:"+cacheKey,60).exec(function(err,results){ _this.debug("cache: "+JSON.stringify(err)+JSON.stringify(results));});
-                    _this.debug("Verification passed for token "+cacheKey+", cached for 60s");
+                    _this.redisClient.multi().set("access_token:" + cacheKey, 'OK').expire("access_token:" + cacheKey, 60).exec(function (err, results) {
+                        _this.debug("cache: " + JSON.stringify(err) + JSON.stringify(results));
+                    });
+                    _this.debug("Verification passed for token " + cacheKey + ", cached for 60s");
                     next(null, "verified_by_persona");
-                }
-                else
-                {
-                    _this.debug("Verification failed for token "+cacheKey+" with status code "+oauthResp.statusCode);
+                } else {
+                    _this.debug("Verification failed for token " + cacheKey + " with status code " + oauthResp.statusCode);
                     res.status(401);
-                    res.set("Connection","close");
-                    res.json({"error":"invalid_token","error_description":"The token is invalid or has expired"});
+                    res.set("Connection", "close");
+                    res.json({
+                        "error": "invalid_token",
+                        "error_description": "The token is invalid or has expired"
+                    });
                 }
-            }).on("error",function(e) {
-                    _this.error("OAuth::validateToken problem: "+ e.message);
-                    res.status(500);
-                    res.set("Connection","close");
-                    res.json({"error":"unexpected_error","error_description":e.message});
-                }).end();
+            }).on("error", function (e) {
+                _this.error("OAuth::validateToken problem: " + e.message);
+                res.status(500);
+                res.set("Connection", "close");
+                res.json({
+                    "error": "unexpected_error",
+                    "error_description": e.message
+                });
+            }).end();
         }
     });
 
 };
 
 PersonaClient.prototype.getToken = function (req) {
-    if (req.header("Authorization"))
-    {
+    if (req.header("Authorization")) {
         var result = req.header("Authorization").match(/Bearer\s(\S+)/);
-        if (result && result.length>1) { return result[1]; }
+        if (result && result.length > 1) {
+            return result[1];
+        }
     }
-    if (req.param('access_token'))
-    {
+    if (req.param('access_token')) {
         return req.param('access_token');
     }
     return null;
@@ -147,29 +155,30 @@ PersonaClient.prototype.getToken = function (req) {
  * @param message
  * @returns {boolean}
  */
-PersonaClient.prototype.log = function(severity, message) {
-    if(!this.config.enable_debug) { return true; }
+PersonaClient.prototype.log = function (severity, message) {
+    if (!this.config.enable_debug) {
+        return true;
+    }
 
-    if(this.config.logger){
-        if(severity === DEBUG){
+    if (this.config.logger) {
+        if (severity === DEBUG) {
             this.config.logger.debug("[persona_client] " + message);
-        } else if ( severity === ERROR ){
+        } else if (severity === ERROR) {
             this.config.logger.error("[persona_client] " + message);
         } else {
-            console.log(severity +": [persona_client] " + message);
+            console.log(severity + ": [persona_client] " + message);
         }
     } else {
-        console.log(severity +": [persona_client] " + message);
+        console.log(severity + ": [persona_client] " + message);
     }
 };
 
-PersonaClient.prototype.debug = function(message) {
+PersonaClient.prototype.debug = function (message) {
     this.log(DEBUG, message);
 };
-PersonaClient.prototype.error = function(message) {
+PersonaClient.prototype.error = function (message) {
     this.log(ERROR, message);
 };
-
 
 /**
  * The only way to get an instance of the Persona Client is through
@@ -177,6 +186,6 @@ PersonaClient.prototype.error = function(message) {
  * @param config
  * @returns {PersonaClient}
  */
-exports.createClient = function(config) {
+exports.createClient = function (config) {
     return new PersonaClient(config);
 };
