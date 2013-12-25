64de1eea888567ac7529e4956418b11a0a6bb4d7
Nadeem Shabir <nadeem.shabir@talis.com>
2013-12-25 21:35:58 +0000

    added more jshint settings and tidied up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,45 +3,44 @@
 module.exports = function (grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-    mochaTest: {
-      test: {
-          options: {
-              reporter: "spec",
-              timeout: 10000
-          },
-          src: ["test/**/*.js"]
-      }
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['index.js']
-      }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'mochaTest']
-      }
-    }
-  });
+    grunt.initConfig({
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: "spec",
+                    timeout: 10000
+                },
+                src: ["test/**/*.js"]
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib: {
+                src: ['index.js']
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib: {
+                files: '<%= jshint.lib.src %>',
+                tasks: ['jshint:lib', 'mochaTest']
+            }
+        }
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'mochaTest']);
-
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'mochaTest']);
 };
