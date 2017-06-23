264121a26f88aaea3a160694b2cafbfd4d360834
Mark Wallsgrove <mark.wallsgrove+git@gmail.com>
2017-06-23 13:19:11 +0100

    test key format is different per config

--- test/persona_client_tests.js
+++ test/persona_client_tests.js
@@ -45,6 +45,33 @@ describe("Persona Client Test Suite - Constructor & Token Tests", function() {
         });
     });
 
+
+    describe("- Key format tests", function() {
+        it("should postfix key", function(done) {
+            var firstPersonaClient = persona.createClient("test-suite", {
+                persona_host: process.env.PERSONA_TEST_HOST || "persona",
+                persona_port: process.env.PERSONA_TEST_PORT || 80,
+                persona_scheme: process.env.PERSONA_TEST_SCHEME || "http",
+                persona_oauth_route: "/oauth/tokens/",
+                enable_debug: false,
+                cert_background_refresh: false,
+            });
+
+            var secondPersonaClient = persona.createClient("test-suite", {
+                persona_host: process.env.PERSONA_TEST_HOST || "persona",
+                persona_port: process.env.PERSONA_TEST_PORT || 81,
+                persona_scheme: process.env.PERSONA_TEST_SCHEME || "http",
+                persona_oauth_route: "/oauth/tokens/",
+                enable_debug: false,
+                cert_background_refresh: false,
+            });
+
+            firstPersonaClient._formatCacheKey('id').should.not.equal(
+                secondPersonaClient._formatCacheKey('id')
+            );
+        })
+    });
+
     describe("- Generate token tests", function() {
         var clock;
         var oauthClient = process.env.PERSONA_TEST_OAUTH_CLIENT || "primate";
